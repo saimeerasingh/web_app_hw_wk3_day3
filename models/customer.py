998d6d987dc4237d926f7f5d07e71d10abdf80b3
saimeerasingh <singhsaimeera@gmail.com>
2021-12-01 13:37:07 +0000

    customer class and object added

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__(self,name,order_date,quantity):
+        self.name = name
+        self.order_date = order_date
+        self.quantity =quantity
+
+        
---
+++ models/customer_details.py
@@ -0,0 +1,6 @@
+from models.customer import *
+
+customer1 = Customer('Penny',30/11/2021,5)
+customer2 = Customer('Amy',29/11/2021,8)
+customer3 = Customer('Bernie',1/12/2021,6)
+customers = [customer1,customer2,customer3]
